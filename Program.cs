c454cd231dfbc2de0ca86166ae91bc41f14a1839
Prajna0408 <59387292+Prajna0408@users.noreply.github.com>
2021-01-11 21:40:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH!");
+            Console.WriteLine("Please consider my code which i've learnt from MLH!!");
+        }
+    }
+}
